3b751c3e8a977135eef4c4d178f27b3f92cd3133
Mgahed <abdelrhmanmgahed131@gmail.com>
2021-04-02 16:52:27 +0200

    Login form and make seeder

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,4 +1,23 @@
 @extends('master')
 @section('content')
-    <h1>Login</h1>
+    <div class="container">
+        <center>
+            <h1><u>Login</u></h1>
+        </center>
+        <div class="row">
+            <div class="col-md-5 offset-md-4">
+                <form>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
